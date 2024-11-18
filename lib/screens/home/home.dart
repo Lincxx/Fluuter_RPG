67eb3f29fc07f3e318bb1b997dfbe175c9cb4d95
Lincxx <bossco28@gmail.com>
2024-11-18 08:33:17 -0500

    Add - Container Gradient

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_rpg/shared/styled_button.dart';
 import 'package:flutter_rpg/shared/styled_text.dart';
 
 class Home extends StatefulWidget {
@@ -25,7 +26,8 @@ class _HomeState extends State<Home> {
             ),
             const StyledHeading('Chatacter List'),
             const StyledTitle('Chatacter List'),
-            FilledButton(onPressed: () {}, child: const Text('Create New'))
+            StyledButton(
+                onPressed: () {}, child: const StyledHeading('Create New'))
           ],
         ),
       ),
