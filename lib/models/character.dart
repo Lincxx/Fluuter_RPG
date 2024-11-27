1585acbb77abd281d3bd6d775d72ddaf102fc9a5
Lincxx <bossco28@gmail.com>
2024-11-27 10:00:17 -0500

    Adding Dummy Data

--- lib/models/character.dart
+++ lib/models/character.dart
@@ -6,7 +6,7 @@ class Character with Stats {
   //constructor
   Character(
       {required this.name,
-      required this.solgan,
+      required this.slogan,
       required this.vocation,
       required this.id});
 
@@ -14,7 +14,7 @@ class Character with Stats {
   final Set<Skill> skills = {};
   final Vocation vocation;
   final String name;
-  final String solgan;
+  final String slogan;
   final String id;
   bool _isFav = false;
 
@@ -31,3 +31,24 @@ class Character with Stats {
     skills.add(skill);
   }
 }
+
+// dummy character data
+List<Character> characters = [
+  Character(
+      id: '1', name: 'Klara', vocation: Vocation.wizard, slogan: 'Kaumf!'),
+  Character(
+      id: '2',
+      name: 'Jonny',
+      vocation: Vocation.junkie,
+      slogan: 'Light me up...'),
+  Character(
+      id: '3',
+      name: 'Crimson',
+      vocation: Vocation.raider,
+      slogan: 'Fire in the hole!'),
+  Character(
+      id: '4',
+      name: 'Shaun',
+      vocation: Vocation.ninja,
+      slogan: 'Alright then gang.'),
+];
