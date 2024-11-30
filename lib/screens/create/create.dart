f52b36b00b39680427b089d6bedd2fc4b0ff0a15
Lincxx <bossco28@gmail.com>
2024-11-30 08:48:54 -0500

    Display Vocations

--- lib/screens/create/create.dart
+++ lib/screens/create/create.dart
@@ -1,6 +1,8 @@
 // create screen widget
 
 import 'package:flutter/material.dart';
+import 'package:flutter_rpg/models/vocation.dart';
+import 'package:flutter_rpg/screens/create/vocation_card.dart';
 import 'package:flutter_rpg/shared/styled_button.dart';
 import 'package:flutter_rpg/shared/styled_text.dart';
 import 'package:flutter_rpg/theme.dart';
@@ -93,6 +95,32 @@ class _CreateState extends State<Create> {
             ),
             const SizedBox(
               height: 30,
+            ),
+            // Select vocation title
+            const Center(
+              child: Icon(Icons.code, color: AppColors.primaryColor),
+            ),
+            const Center(
+              child: StyledHeading('Choose a Vocation'),
+            ),
+            const Center(
+              child: StyledText('This determines your available skills'),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            // vocation cards
+            const VocationCard(
+              vocation: Vocation.junkie,
+            ),
+            const VocationCard(
+              vocation: Vocation.ninja,
+            ),
+            const VocationCard(
+              vocation: Vocation.raider,
+            ),
+            const VocationCard(
+              vocation: Vocation.wizard,
             ),
             Center(
                 child: StyledButton(
