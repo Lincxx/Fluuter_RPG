acc0a74ea026053470a52a9251b149fa3b1d16fc
Lincxx <bossco28@gmail.com>
2024-11-30 08:50:29 -0500

    Adding Scrollable widget

--- lib/screens/create/create.dart
+++ lib/screens/create/create.dart
@@ -52,82 +52,89 @@ class _CreateState extends State<Create> {
       ),
       body: Container(
         padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 30),
-        child: Column(
-          children: [
-            //welcome message
-            const Center(
-              child: Icon(Icons.code, color: AppColors.primaryColor),
-            ),
-            const Center(
-              child: StyledHeading('Welcome new player!'),
-            ),
-            const Center(
-              child: StyledText('Create a name & slogan for your character'),
-            ),
-            const SizedBox(
-              height: 30,
-            ),
-            //input fields
-            TextField(
-              controller: _nameController,
-              style: GoogleFonts.kanit(
-                  textStyle: Theme.of(context).textTheme.bodyMedium),
-              cursorColor: AppColors.textColor,
-              decoration: const InputDecoration(
-                  prefixIcon: Icon(
-                    Icons.person_2,
-                  ),
-                  label: StyledText('Character Name')),
-            ),
-            const SizedBox(
-              height: 30,
-            ),
-            TextField(
-              controller: _sloganController,
-              style: GoogleFonts.kanit(
-                  textStyle: Theme.of(context).textTheme.bodyMedium),
-              cursorColor: AppColors.textColor,
-              decoration: const InputDecoration(
-                  prefixIcon: Icon(
-                    Icons.chat_bubble,
-                  ),
-                  label: StyledText('Character Slogan')),
-            ),
-            const SizedBox(
-              height: 30,
-            ),
-            // Select vocation title
-            const Center(
-              child: Icon(Icons.code, color: AppColors.primaryColor),
-            ),
-            const Center(
-              child: StyledHeading('Choose a Vocation'),
-            ),
-            const Center(
-              child: StyledText('This determines your available skills'),
-            ),
-            const SizedBox(
-              height: 30,
-            ),
-            // vocation cards
-            const VocationCard(
-              vocation: Vocation.junkie,
-            ),
-            const VocationCard(
-              vocation: Vocation.ninja,
-            ),
-            const VocationCard(
-              vocation: Vocation.raider,
-            ),
-            const VocationCard(
-              vocation: Vocation.wizard,
-            ),
-            Center(
-                child: StyledButton(
-              onPressed: handelSubmit,
-              child: const StyledHeading('Create Character'),
-            ))
-          ],
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              //welcome message
+              const Center(
+                child: Icon(Icons.code, color: AppColors.primaryColor),
+              ),
+              const Center(
+                child: StyledHeading('Welcome new player!'),
+              ),
+              const Center(
+                child: StyledText('Create a name & slogan for your character'),
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+
+              //input fields
+              TextField(
+                controller: _nameController,
+                style: GoogleFonts.kanit(
+                    textStyle: Theme.of(context).textTheme.bodyMedium),
+                cursorColor: AppColors.textColor,
+                decoration: const InputDecoration(
+                    prefixIcon: Icon(
+                      Icons.person_2,
+                    ),
+                    label: StyledText('Character Name')),
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              TextField(
+                controller: _sloganController,
+                style: GoogleFonts.kanit(
+                    textStyle: Theme.of(context).textTheme.bodyMedium),
+                cursorColor: AppColors.textColor,
+                decoration: const InputDecoration(
+                    prefixIcon: Icon(
+                      Icons.chat_bubble,
+                    ),
+                    label: StyledText('Character Slogan')),
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+
+              // Select vocation title
+              const Center(
+                child: Icon(Icons.code, color: AppColors.primaryColor),
+              ),
+              const Center(
+                child: StyledHeading('Choose a Vocation'),
+              ),
+              const Center(
+                child: StyledText('This determines your available skills'),
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+
+              // vocation cards
+              const VocationCard(
+                vocation: Vocation.junkie,
+              ),
+              const VocationCard(
+                vocation: Vocation.ninja,
+              ),
+              const VocationCard(
+                vocation: Vocation.raider,
+              ),
+              const VocationCard(
+                vocation: Vocation.wizard,
+              ),
+
+              // submit button
+              Center(
+                  child: StyledButton(
+                onPressed: handelSubmit,
+                child: const StyledHeading('Create Character'),
+              ))
+            ],
+          ),
         ),
       ),
     );
