5278cbc72caf58b98a0628b2dd82626e88cbade8
Lincxx <bossco28@gmail.com>
2024-12-04 15:35:46 -0500

    Fleshing out the profile screen

--- lib/screens/profile/profile.dart
+++ lib/screens/profile/profile.dart
@@ -1,22 +1,80 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_rpg/models/character.dart';
 import 'package:flutter_rpg/shared/styled_text.dart';
+import 'package:flutter_rpg/theme.dart';
 
 class Profile extends StatelessWidget {
-  const Profile({super.key});
+  const Profile({super.key, required this.character});
+  final Character character;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const StyledTitle('Character name'),
+        title: StyledTitle(character.name),
       ),
-      body: const SingleChildScrollView(
+      body: SingleChildScrollView(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            // basic info
-
+            // basic info, image, vocation, description
+            Container(
+              padding: const EdgeInsets.all(16),
+              color: AppColors.secondaryColor.withOpacity(0.3),
+              child: Row(
+                children: [
+                  Image.asset(
+                    'assets/img/vocations/${character.vocation.image}',
+                    width: 140,
+                    height: 140,
+                  ),
+                  const SizedBox(
+                    width: 20,
+                  ),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        StyledHeading(character.name),
+                        StyledText(character.vocation.description),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
             // weapon and ability
+            const SizedBox(
+              height: 20,
+            ),
+            const Center(
+              child: Icon(
+                Icons.code,
+                color: AppColors.primaryColor,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16),
+              child: Container(
+                width: double.infinity,
+                padding: const EdgeInsets.all(16),
+                color: AppColors.secondaryColor.withOpacity(0.5),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const StyledHeading('Slogan'),
+                    StyledText(character.slogan),
+                    const SizedBox(height: 10),
+                    const StyledHeading('Weapon'),
+                    StyledText(character.vocation.weapon),
+                    const SizedBox(height: 10),
+                    const StyledHeading('Unique'),
+                    StyledText(character.vocation.ability),
+                    const SizedBox(height: 10),
+                  ],
+                ),
+              ),
+            ),
 
             // stata & skills
 
