95f1b9bf6a8365f49fed40dca652d5890fdecf01
Lincxx <bossco28@gmail.com>
2024-12-05 10:20:57 -0500

    Outputting Vocation Skills

--- lib/screens/profile/skill_list.dart
+++ lib/screens/profile/skill_list.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_rpg/models/character.dart';
+import 'package:flutter_rpg/models/skill.dart';
 import 'package:flutter_rpg/shared/styled_text.dart';
 import 'package:flutter_rpg/theme.dart';
 
@@ -12,6 +13,17 @@ class SkillList extends StatefulWidget {
 }
 
 class _SkillListState extends State<SkillList> {
+  late List<Skill> availableSkills;
+
+  //lifecyle initstate
+  @override
+  void initState() {
+    availableSkills = allSkills.where((skill) {
+      return skill.vocation == widget.character.vocation;
+    }).toList();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -19,15 +31,27 @@ class _SkillListState extends State<SkillList> {
       child: Container(
         padding: const EdgeInsets.all(16),
         color: AppColors.secondaryColor.withOpacity(0.5),
-        child: const Column(
+        child: Column(
           children: [
-            StyledHeading('Choose an active skill'),
-            StyledText('Skills are unique to your vocation'),
-            SizedBox(
+            const StyledHeading('Choose an active skill'),
+            const StyledText('Skills are unique to your vocation'),
+            const SizedBox(
               height: 20,
             ),
             Row(
-              children: [],
+              children: availableSkills.map((skill) {
+                return Container(
+                  margin: const EdgeInsets.all(5),
+                  padding: const EdgeInsets.all(2),
+                  child: Image.asset(
+                    'assets/img/skills/${skill.image}',
+                    width: 70,
+                  ),
+                );
+              }).toList(),
+            ),
+            const SizedBox(
+              height: 10,
             ),
           ],
         ),
