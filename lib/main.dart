88e3798e6b207b877a23a7e6827199fdafc41b7c
Lincxx <bossco28@gmail.com>
2024-12-06 09:24:19 -0500

    Firebase setup

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,18 @@ import 'package:flutter_rpg/screens/home/home.dart';
 import 'package:flutter_rpg/services/character_store.dart';
 import 'package:flutter_rpg/theme.dart';
 import 'package:provider/provider.dart';
+//firebase
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+// ...
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(ChangeNotifierProvider(
     create: (context) => CharacterStore(),
     child: MaterialApp(
